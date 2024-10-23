32db77c37dded1cca6b5fbd0c5801978ec6a0fab
Blanzy124 <samuelmedinablandon124@gmail.com>
2024-10-22 23:39:45 -0700

    still working

--- apppropia.mjs
+++ apppropia.mjs
@@ -1,28 +1,32 @@
+import fss from 'node:fs'
 import fs from 'node:fs/promises';
 import path from 'node:path';
- console.log('ejecutando...');
+console.log('ejecutando...');
  
- const folder = path.join('C:', 'Users', 'samue', 'OneDrive', 'Desktop', 'back');
- const folderName = path.basename(folder)
- const folderStatus = await fs.stat(folder)
-
-fs.readdir(folder)
-.then(files =>{
- files.forEach(file =>{
-  Promise.all(file =>{
-   fs.readFile(file, 'utf-8')
+const folder = path.join('C:', 'Users', 'samue', 'OneDrive', 'Desktop', 'back');
+const folderName = path.basename(folder)
+try{
+ fs.readdir(folder).then(files =>{ //filtro de una carpeta vacia
+  if(files == 0){
+   console.log(`no hay nada en "${folderName}"`)
+   return;
+  };
+  files.forEach(file =>{
+   let filesLocation = path.join(folder, file)
+   fs.readFile(filesLocation).then
   })
-  
+ 
  })
-})
+}catch{
+ console.error(`algo salio mal`)
+ process.exit(1)
+}
 
- try{
 
- }catch{
 
-  console.error(`no hay o no son lejibles los elementos en tu carpeta ${folderName}`);
-  process.exit(1)
- }
+ 
+ 
+
 
 //BUSCAR LA MANERA DE PODER USAR EL READFILE JUNTO CON AWAIT
  
