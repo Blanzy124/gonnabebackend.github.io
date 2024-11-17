6f50b9f4bce149fb665823ac8480a860a04fe515
Blanzy124 <samuelmedinablandon124@gmail.com>
2024-11-17 14:18:53 -0800

    making interactive app

--- app.mjs
+++ app.mjs
@@ -6,47 +6,57 @@ import { folderName } from './appCreateFiles.mjs';
 import { creation } from './appCreateFiles.mjs';
 
 
-console.log(`programa inicializado...`)
+console.log(`starting...`)
+
+
+
+
+
+
+
 
 const rl = readLine.createInterface({
  input: process.stdin,
  output: process.stdout
 });
 
-rl.question(`cuantos archivos deseas crear?`, (cantidad) => {
+let option;
 
- creation(cantidad)
-
-
-
-
+console.log(`What you like to do?`)
+console.log(`1----create files.`)
+console.log(`2----Read and have the total of the created files.`)
+console.log(`3----Delete files.`)
+console.log(`-----------------------------------`)
+/////////////////////////////
+rl.question(`Selection =  `, (options) => {
+ option = parseInt(options)
+ rl.question(`Who many file you what to create?` , (quantityy) => {
+  quantity = parseInt(quantityy)
+  creation(quantity)
+  console.log(`algo`)
+  rl.close()
+ })
  rl.close();
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-
-
-
- 
 })
+
+
+//switch (option) {
+// case 1:
+// }
+//  
+//  break;
+//
+// default:
+//  break;
+//}
+
+
+
+
+
+
+
+
 
 
 
