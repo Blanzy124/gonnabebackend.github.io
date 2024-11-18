c8ec4543cc36025a3406922e8de1a4e0f4bcf1f8
Blanzy124 <samuelmedinablandon124@gmail.com>
2024-11-17 16:48:52 -0800

    trabajando...

--- app.mjs
+++ app.mjs
@@ -4,54 +4,18 @@ import path from 'node:path';
 import readLine from 'readline';
 import { folderName } from './appCreateFiles.mjs';
 import { creation } from './appCreateFiles.mjs';
+import inquirer from 'inquirer';
+import { type } from 'node:os';
 
 
 console.log(`starting...`)
 
-
-
-
-
-
-
-
-const rl = readLine.createInterface({
- input: process.stdin,
- output: process.stdout
-});
-
-let option;
-
-console.log(`What you like to do?`)
-console.log(`1----create files.`)
-console.log(`2----Read and have the total of the created files.`)
-console.log(`3----Delete files.`)
-console.log(`-----------------------------------`)
-/////////////////////////////
-rl.question(`Selection =  `, (options) => {
- option = parseInt(options)
- rl.question(`Who many file you what to create?` , (quantityy) => {
-  quantity = parseInt(quantityy)
-  creation(quantity)
-  console.log(`algo`)
-  rl.close()
- })
- rl.close();
-})
-
-
-//switch (option) {
-// case 1:
-// }
-//  
-//  break;
-//
-// default:
-//  break;
-//}
-
-
-
+const start = [
+ {
+  type: 'input',
+  
+ }
+]
 
 
 
