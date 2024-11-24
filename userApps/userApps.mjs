1fc31908e6d35bc3cbd7428c5f21b669ca06657e
Blanzy124 <samuelmedinablandon124@gmail.com>
2024-11-23 19:41:48 -0800

    creacion de nombre de usuario y verificacion completada

--- userApps/userApps.mjs
+++ userApps/userApps.mjs
@@ -1,17 +1,57 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import { create } from './userCreation.mjs';
-export var folder = path.join('C:', 'Users', 'samue', 'OneDrive', 'Desktop', 'companyUsers')
-
-var uName = `Samuel Blandon`
-var pass = `2005`
-var da = `11/22/2024`
-
-create(uName, pass, da)
+import inquirer from 'inquirer';
+export var folder = path.join('C:', 'Users', 'samue', 'OneDrive', 'Desktop', 'companyUsers');
 
 
+let tOf;
+
+function verifyUserExist(userName, dataBase){
+ return dataBase.includes(`${userName}.txt`)
+}
+function nameQuestion(){
+
+ inquirer.prompt([{
+  type: "input",
+  name: 'userName',
+  message: "what Is Your Name?"
+ }]).then(({userName}) => {
+  fs.readdir(folder, (err, dataBase)  => {
+   if(err){
+    console.error(`Error`)
+    tOf = false;
+   }
 
 
+   if(verifyUserExist(userName, dataBase)){
+    console.error(`el usuario ya existe, escribelo otro por favor`)
+    nameQuestion()
+
+    return;
+   }
+   else{
+     inquirer.prompt([{
+       type: "password",
+       name: 'password',
+       mask: '*',
+       message: "Escribe tu contrasena"
+      }]).then(({password}) => {
+       create(userName, password, new Date().toLocaleString())
+      })
+   }
+
+
+   
+  })
+})
+}   
+ 
+ 
+ 
+
+
+nameQuestion()
 
 
 
